611323a1ba34dcab37ccea5e86612c556cc56d46
Alain <neroliabali@gmail.com>
2026-08-07 16:43:48 +0200

    Intermediate commit - UC Admin, round 4

--- UC_Admin.py
+++ UC_Admin.py
@@ -1,5 +1,7 @@
+import datetime
+
 from UC_Bettor import tournament_status, calculate_score, E_PREDICTIONS, E_OUTCOMES
-from UC_Time import input_outcome
+from UC_Time import input_outcome, display_bettable
 from model import tournament
 from model.bet import Bet
 from model.bettable import Bettable
@@ -8,6 +10,7 @@ from model.betty import Betty
 from model.participation import Participation
 from model.phase import Phase
 from model.ranking import Ranking
+from model.team import Team
 from model.tournament import Tournament
 from ui.console_ui import input_selection, info
 
@@ -43,10 +46,38 @@ class UiAdminContext:
 Admin Use Cases implementation
 """
 
+""" _______________ TOURNAMENT SETUP _______________ """
+
 """
 UC Tournament Creation:
 The admin creates the tournament, setting its name, start and end date
 """
+
+"""
+UC Phase Creation:
+PRE tournament is selected
+The admin creates a tournament phase, setting its type, name, number, wallet
+"""
+
+"""
+UC Bettable Creation:
+PRE tournament T is selected, T phase is selected
+The admin creates a bettable event for the selected phase, setting the teams involved and its start date & time. 
+If the start date and time is in the future, the bettable state is set to OPEN 
+"""
+
+"""
+UC Team Creation:
+The admin creates a bettable team, setting its name
+"""
+
+"""
+UC Team Tournament assignment;
+PRE tournament is selected
+The admin assigns a bettable team to a tournament, setting its sheep price for the tournament
+"""
+
+""" _______________ TOURNAMENT OPERATION _______________ """
 
 def tournament_selection(states:list[str]=None) -> int:
     """
@@ -71,39 +102,43 @@ def tournament_selection(states:list[str]=None) -> int:
         return selection
     return -1
 
-"""
-UC Team Creation:
-The admin creates a bettable team, setting its name
-"""
-
-"""
-UC Team Tournament assignment;
-PRE tournament is selected
-The admin assigns a bettable team to a tournament, setting its sheep price for the tournament
-"""
-
-"""
-UC Phase Creation:
-PRE tournament is selected
-The admin creates a tournament phase, setting its type, name, number, wallet
-"""
-
-"""
-UC Bettable Creation:
-PRE tournament T is selected, T phase is selected
-The admin creates a bettable event for the selected phase, setting the teams involved and its start date & time. 
-If the start date and time is in the future, the bettable state is set to OPEN 
-"""
-
-"""
-UC Tournament Publishing:
-PRE tournament is selected
-The admin opens the tournament to bettors. The tournament's web-site is created, an invitation e-mail wih the link is sent
-to the registered bettors (+ filtering +), and possibly to additional bettor candidates
-"""
 def open_tournament():
-    pass
+    """
+    UC Tournament Publishing:
+    PRE tournament is selected
+    The admin opens the tournament to bettors. The tournament's web-site is created, an invitation e-mail wih the link is sent
+    to the registered bettors (+ filtering +), and possibly to additional bettor candidates
+    """
+    print("<<<< Not implemented >>>>")
 
+def betty_status():
+    """
+    UC Betty status
+    Existing tournaments are listed together with their information.
+    Namely status, start date, end date, #participants and for each phase: its status, start date, %age completion, #bettables, #bets.
+    """
+    tr_attr_dicts = Betty().query(
+        f"SELECT id, name, start_dt, end_dt, state  "
+        f"FROM {Tournament._table_} tr "
+        f"ORDER BY start_dt DESC")
+
+    for tr_attr_dict in tr_attr_dicts:
+        print(f"Tournament: {tr_attr_dict['id']} {tr_attr_dict['name']} [{tr_attr_dict['start_dt']} - {tr_attr_dict['end_dt']}] {tr_attr_dict['state']}")
+        bettable_attr_dicts = Betty().query(
+            f"SELECT ba.id as bettable_id, ba.start_dt, t1.name as t1_name, t2.name as t2_name, ba.outcome, ba.state, ph.id as phase_id, ph.name as phase_name, ph.state as phase_state, ph.scoring "
+            f"FROM {Bettable._table_} ba "
+            f"JOIN {Phase._table_} ph ON ba.phase_id = ph.id "
+            f"JOIN {Team._table_} t1 ON ba.a_team_id = t1.id "
+            f"JOIN {Team._table_} t2 ON ba.b_team_id = t2.id "
+            f"WHERE ph.tournament_id = {tr_attr_dict['id']} "
+            f"ORDER BY ba.start_dt ASC")
+        prv_phase_name = None
+        for bettable_attr_dict in bettable_attr_dicts:
+            if (phase_name:=bettable_attr_dict['phase_name']) != prv_phase_name:
+                phase_name = phase_name.strip()
+                prv_phase_name = phase_name
+                print(f"    {phase_name} (Scoring rule: {bettable_attr_dict['scoring']})")
+            print(f"        {bettable_attr_dict['bettable_id']} {bettable_attr_dict['start_dt']} {bettable_attr_dict['state']} {bettable_attr_dict['t1_name']} - {bettable_attr_dict['t2_name']} ")
 
 def compute_ranking():
     """
@@ -118,9 +153,6 @@ def compute_ranking():
             f"FROM {Bettable._table_} b, {Phase._table_} p, {Tournament._table_} tr "
             f"WHERE b.phase_id = p.id AND p.tournament_id = {UiAdminContext().tournament_selected_id} AND b.outcome IS NOT NULL "
             f"ORDER BY b.start_dt ASC")
-
-        prv_score = ''
-        ranking = 1
 
         scoring = dict()
         bet_score = dict()
@@ -167,8 +199,6 @@ def compute_ranking():
             r.save()
 
 
-
-
 def show_ranking():
     """
     UC Tournament ranking:
@@ -194,10 +224,11 @@ def show_ranking():
                 ranking = rank + 1
             print(f"{ranking:3} {attr_dict['nickname']:20} {attr_dict['score']:3} points")
 
-"""
-UC The admin get the list of a tournament's paticipants
-"""
+
 def tournament_participation(silent=False) -> list[(int,str,str,int)]:
+    """
+    UC The admin get the list of a tournament's paticipants
+    """
     if not UiAdminContext().tournament_selected:
         tournament_selection()
     if UiAdminContext().tournament_selected:
@@ -228,34 +259,45 @@ def tournament_participation(silent=False) -> list[(int,str,str,int)]:
                 print(f"   {bet_stat[0]} as {bet_stat[1]} : {bet_stat[2]} bets")
         return result
 
-"""
-UC The admin registers a bettable score after its completion:
-PRE RUNNING tournament T selected, RUNNING bettable selected
-The admin registers the final score of the bettable. The bettable is set to CLOSED. 
-The bettor ranking is re-computed. 
-"""
-
-"""
-UC The admin corrects a bettable score after its completion:
-PRE RUNNING tournament T selected, CLOSED bettable selected
-The admin corrects the final score of the bettable.
-"""
-
 def quit()->int:
     """
     """
     UiAdminContext().tournament_selected_name = None
     return -1
 
+
+def input_bettable_outcome():
+    """
+    UC The admin registers or corrects a bettable score after its completion:
+    PRE RUNNING tournament T selected, CLOSED bettable selected
+    The admin corrects the final score of the bettable.
+    """
+
+    if not UiAdminContext().tournament_selected:
+        tournament_selection()
+    if UiAdminContext().tournament_selected:
+        # Get the bettable of which the start date is overdue and have not outcome
+        attr_dicts = Betty().query(f"SELECT b.id, ta.name AS a_team_name, tb.name AS b_team_name, b.start_dt, b.state, p.name AS phase_name, b.outcome"
+                                   f" FROM {Bettable._table_} b"
+                                   f" JOIN {Team._table_} ta ON ta.id = b.a_team_id"
+                                   f" JOIN {Team._table_} tb ON tb.id = b.b_team_id"
+                                   f" JOIN {Phase._table_} p ON b.phase_id = p.id AND p.tournament_id={UiAdminContext().tournament_selected_id}"
+                                   f" WHERE b.start_dt < '{datetime.datetime.now()}' AND b.outcome IS NULL"
+                                   f" ORDER BY b.start_dt ASC")
+        if attr_dicts:
+            while (selection:=input_selection(attr_dicts, lambda x: display_bettable(x))) >= 0:
+                input_outcome(attr_dicts[selection])
+                compute_ranking()
+
 if __name__ == '__main__':
     #load_dotenv()
     tournament_selection()
-    options = [('Provide/Amend the outcome of a bettable', lambda : input_outcome()),
+    options = [('Provide/Amend the outcome of a bettable', lambda : input_bettable_outcome()),
                ('Open tournament', lambda : open_tournament()),
                ('Tournament participation', tournament_participation),
                ('Compute ranking', lambda: compute_ranking()),
                ('Show ranking', lambda: show_ranking()),
-               ('Status', lambda : tournament_status()),
+               ('Status', lambda : betty_status()),
                ('Quit', lambda : quit())]
     selection = 0
     ret = None
